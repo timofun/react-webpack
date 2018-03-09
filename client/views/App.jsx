f7435be3a186195f4b0c472830bd6d072e1e773e
timofun <timo_quiet@163.com>
2018-03-09 17:33:50 +0800

    增加appBar

--- client/views/App.jsx
+++ client/views/App.jsx
@@ -1,20 +1,15 @@
 import React from 'react'
-import {
-  Link,
-} from 'react-router-dom'
 import Routes from '../config/router'
+import AppBar from './layout/app-bar'
 
 export default class App extends React.Component {
   componentDidMount() {
     // do something here
   }
+
   render() {
     return [
-      <div key="banner">
-        <Link to="/">首页</Link>
-        <br />
-        <Link to="/detail">详细页</Link>
-      </div>,
+      <AppBar />,
       <Routes key="routes"/>,
     ]
   }
